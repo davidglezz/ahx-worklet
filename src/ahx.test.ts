3b85231e40b75882db144a190010e04b5b4ad730
davidglezz <davidg@empathy.co>
2024-02-19 20:20:00 +0100

    test: imprive test concurrency
    Signed-off-by: davidglezz <davidg@empathy.co>

--- src/ahx.test.ts
+++ src/ahx.test.ts
@@ -1,5 +1,5 @@
 import { readFileSync } from 'node:fs';
-import { describe, expect, it } from 'vitest';
+import { describe, it } from 'vitest';
 import {
   dataType as DataType,
   AHXOutput as ReferenceOutput,
@@ -8,8 +8,8 @@ import {
 import { AHXOutput, AHXSong } from './ahx.ts';
 import { dump, toArrayBuffer } from './utils.ts';
 
-describe.concurrent('AHX: It should output the same buffer values', () => {
-  it.each([
+describe('AHX', () => {
+  describe.concurrent.each([
     ['03.ahx'],
     ['04.ahx'],
     ['die audienz ist horenz.ahx'], // Fail
@@ -19,22 +19,24 @@ describe.concurrent('AHX: It should output the same buffer values', () => {
     ['loom.ahx'],
     ['thxcolly-intro.ahx'],
     ['void.ahx'],
-  ])('File %s', file => {
-    const songBytes = readFileSync(`test-songs/${file}`);
+  ])('It should output the same buffer values', file => {
+    it(`File: ${file}`, ({ expect }) => {
+      const songBytes = readFileSync(`test-songs/${file}`);
 
-    const binString = new DataType();
-    binString.data = String.fromCharCode(...songBytes);
-    const referenceSong = new ReferenceSong();
-    referenceSong.InitSong(binString);
-    const expected = dump(ReferenceOutput(), referenceSong);
+      const binString = new DataType();
+      binString.data = String.fromCharCode(...songBytes);
+      const referenceSong = new ReferenceSong();
+      referenceSong.InitSong(binString);
+      const expected = dump(ReferenceOutput(), referenceSong);
 
-    const song = new AHXSong(toArrayBuffer(songBytes));
-    const actual = dump(new AHXOutput(), song);
+      const song = new AHXSong(toArrayBuffer(songBytes));
+      const actual = dump(new AHXOutput(), song);
 
-    //expect(JSON.stringify(song, null, 2)).toEqual(JSON.stringify(referenceSong, null, 2));
-    for (const expectedChunk of expected) {
-      expect(actual.next().value).toEqual(expectedChunk);
-    }
-    expect(actual.next()).toEqual({ value: undefined, done: true });
+      expect(JSON.stringify(song, null, 2)).toEqual(JSON.stringify(referenceSong, null, 2));
+      for (const expectedChunk of expected) {
+        expect(actual.next().value).toEqual(expectedChunk);
+      }
+      expect(actual.next()).toEqual({ value: undefined, done: true });
+    });
   });
 });
