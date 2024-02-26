0ad03e790258f1f0c0d3cd08d9d09256d1142ef3
davidglezz <davidg@empathy.co>
2024-02-26 22:38:59 +0100

    feat: add songlist
    Signed-off-by: davidglezz <davidg@empathy.co>

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,16 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div class="card">
       <button id="play" type="button">Play</button>
     </div>
+    <div class="card">
+      <ul id="songlist"></ul>
+    </div>
   </div>
 `;
+
+let node: AHXNode;
+
+document.querySelector<HTMLButtonElement>('#play')!.onclick = () => play(location.hash.slice(1));
+window.addEventListener('hashchange', () => node && play(location.hash.slice(1)));
 
 async function loadBinary(url: string) {
   const response = await fetch(url);
@@ -19,21 +27,45 @@ async function loadBinary(url: string) {
   return await response.arrayBuffer();
 }
 
-const songUrl = 'https://modland.com/pub/modules/AHX/451/aces%20high.ahx';
-
-document.querySelector<HTMLButtonElement>('#play')!.onclick = async () => {
-  const context = new AudioContext();
-  try {
-    await context.audioWorklet.addModule(AHXProcessor);
-  } catch (err) {
-    console.error(err);
-    return;
-  }
-
+async function start(context: AudioContext) {
+  await context.audioWorklet.addModule(AHXProcessor);
   const node = new AHXNode(context);
   node.connect(context.destination);
   node.port.onmessage = console.log; // eslint-disable-line no-console
   context.resume();
-  const song = await loadBinary(songUrl);
-  node.load(song);
-};
+  return node;
+}
+
+async function play(songName: string) {
+  if (!node) {
+    node = await start(new AudioContext());
+  }
+  if (!songName) {
+    return;
+  }
+  const url = `https://modland.com/pub/modules/AHX/${songName}.ahx`;
+  const songData = await loadBinary(url);
+  await node.load(songData);
+}
+
+async function loadSongList() {
+  const response = await fetch('songlist.txt');
+  const data = await response.text();
+  return data.split('\n');
+}
+
+async function displaySongList() {
+  const list = document.querySelector<HTMLUListElement>('#songlist')!;
+  const songs = await loadSongList();
+  songs.forEach(song => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    a.dataset.song = song;
+    a.textContent = song;
+    a.href = `#${encodeURIComponent(song)}`;
+    li.appendChild(a);
+    list.appendChild(li);
+  });
+}
+
+displaySongList();
